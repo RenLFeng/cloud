155625ecfb9193b1edd37c5ecec0f36c73e4e461
songyanhui <songyanhui@exsoft.com.cn>
2019-12-03 16:32:51 +0800

    修改评测页面

--- v1.0.1/src/util.js
+++ v1.0.1/src/util.js
@@ -85,3 +85,15 @@ export const pingceType = (v) => {
       break;
   }
 }
+export const parseURL = (url) =>{
+  var query = url && url.split('?')[1]
+  var queryArr = query && query.split('&')
+  var params = {}
+  queryArr &&
+    queryArr.forEach(function (item) {
+      var key = item.split('=')[0]
+      var value = item.split('=')[1]
+      params[key] = value
+    })
+  return params
+}
