4e92fcc418aa42313f578ad5319fd973aae5d8b8
songyanhui <songyanhui@exsoft.com.cn>
2020-03-24 18:38:31 +0800

    微信 登录

--- v1.0.1/src/views/Login.vue
+++ v1.0.1/src/views/Login.vue
@@ -125,23 +125,6 @@ export default {
     weixinLogin() {
       this.popupWeiXxinLogin = true;
     },
-      const s = document.createElement("script");
-      s.type = "text/javascript";
-      s.src = "https://res.wx.qq.com/connect/zh_CN/htmledition/js/wxLogin.js";
-      s.id = "wechat";
-      const wxElement = document.body.appendChild(s);
-      wxElement.onload = function() {
-        let obj = new WxLogin({
-          id: "weixinLogin", // 需要显示的容器id
-          appid: "wx40632058fe27bbb6", // 公众号appid wx*******
-          scope: "snsapi_login", // 网页默认即可
-          redirect_uri: encodeURIComponent("https://www2.exsoft.com.cn"), // 授权成功后回调的url
-          state: Math.ceil(Math.random() * 1000), // 可设置为简单的随机数加session用来校验
-          style: "black", // 提供"black"、"white"可选。二维码的样式
-          href: "" // 外部css文件url，需要https
-        });
-      };
-    },
     selectFn(state) {
       this.state = state;
     },
